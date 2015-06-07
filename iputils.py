01894e605a2f4831130af9d27479ba23db333ee7
adamacosta <acosta.adam@gmail.com>
2015-06-07 11:21:18 -0500

    added documentation and helper functions to iputils.py

--- iputils.py
+++ iputils.py
@@ -1,52 +1,148 @@
+"""Utilities for handling IP addresses.
+
+This module provides classes, data structures, and parsers to make it 
+easy to manipulate, match, and route IP addresses.
+"""
+
 import re
 from numpy import binary_repr
 from bitarray import bitarray
 
 
+def _gen_octet(bits):
+	exec('b = ' + '0b' + bits)
+	return str(b)
+
+def _bits_to_string(bits):
+	return bits.to01()
+
+def _bits_to_dotted_quad(bits):
+	return '.'.join(list(map(_gen_octet, [bits[:8].to01(), 
+		                                  bits[8:16].to01(), 
+	                                      bits[16:24].to01(), 
+	                                      bits[24:32].to01()])))
+
+def _dotted_quad_to_bits(quad):
+	return bitarray(''.join(list(map(binary_repr,
+		                    list(map(int, quad.split('.'))), [8] * 4))))
+
+def _is_valid_ip(addr):
+	assert(re.match('([0-9]{1,3}\.){3}[0-9]{1,3}', addr))
+	assert(sum([int(i)>=0 and int(i)<=255 for i in addr.split('.')]))
+	return True
+
+def _is_valid_cidr(addr):
+	assert(re.match('([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{2}', addr))
+	assert(_is_valid_ip(addr.split('/')[0]))
+	assert(int(addr.split('/')[1])>=0 and int(addr.split('/')[1]) <=32)
+	return True
+
+
 class IP:
+	"""Return an IP object.
+
+	Attributes
+	----------
+	bits -- A 32-bit bitarray formed by concatenating the binary 
+	    representations of each octet of the IP address.
+
+	Methods
+	-------
+	getbits() -> bitarray
+	tostring() -> string
+	tobitstring() -> string
+	lpm() -> int
+	"""
 	def __init__(self, addr):
-		assert(re.match('([0-9]{1,3}\.){3}[0-9]{1,3}', addr))
-		self.ip = addr
-		self.bits = bitarray(''.join(list(map(binary_repr, 
-			                         list(map(int, addr.split('.'))), [8]*4))))
+		"""Initialize an IP object.
+
+		The argument to IP must be a string in dotted quad format
+		consisting of four integers in the range 0 to 255, 
+		separated by a single dot.
+		"""
+		assert(_is_valid_ip(addr))
+		self.bits = _dotted_quad_to_bits(addr)
+
+	def getbits(self):
+		"""Return the 32-bit bitarray representing the IP address."""
+		return self.bits
 
 	def tostring(self):
-		return self.ip
+		"""Return a string representation in dotted quad format."""
+		return _bits_to_dotted_quad(self.bits)
 
-	def tobits(self):
-		return ''.join(self.bits.decode({'1':bitarray('1'), 
-			                             '0':bitarray('0')}))
+	def tobitstring(self):
+		"""Return a 32-bit string showing hardware layout of address."""
+		return _bits_to_string(self.bits)
 
 	def lpm(self, other):
+		"""Return the longest prefix match with a CIDR block."""
 		assert(isinstance(other, CIDRBlock))
-		if (self.bits ^ other.ip.bits).index(True) >= other.mask:
+		if (self.bits ^ other.first).index(True) >= other.mask:
 			return other.mask
 		else:
 			return 0
 
 class CIDRBlock:
 	def __init__(self, addr):
-		assert(re.match('([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{2}', addr))
-		self.ip = IP(addr.split('/')[0])
+		assert(_is_valid_cidr(addr))
+		self.first = _dotted_quad_to_bits(addr.split('/')[0])
 		self.mask = int(addr.split('/')[1])
+		self.bitmask = bitarray('1'*self.mask + '0'*(32 - self.mask))
+		self.last = self.first | ~self.bitmask
 
 	def tostring(self):
-		return self.ip.tostring() + '/' + str(self.mask)
+		"""Return string representation in CIDR format."""
+		return _bits_to_dotted_quad(self.first) + '/' + str(self.mask)
 
-	def tobits(self):
-		return self.ip.tobits()
+	def getrange(self):
+		"""Return the first and last address in the block."""
+		return _bits_to_dotted_quad(self.first), _bits_to_dotted_quad(self.last)
+
+	def getmask(self):
+		"""Return the subnet mask associated with this CIDR block."""
+		return _bits_to_dotted_quad(self.bitmask)
 
 	def matches(self, other):
+		"""Return True if IP address is in block."""
 		assert(isinstance(other, IP))
-		return self.ip.tobits()[:self.mask] == other.tobits()[:self.mask]
+		return self.first[:self.mask] == other.getbits()[:self.mask]
 
 class trie:
 	# TODO: implement a trie
 	pass
 
 def ip_parser(addr):
+	"""Try to parse a string in dotted quad format.
+
+	Arguments
+	---------
+	addr -- A string representing an IP address.
+
+	Returns
+    -------
+	IP -- An IP object is returned if the input string has no trailing
+	    slash and mask.
+	CIDRBlock -- A CIDRBlock object is returned if the input string does
+	    have a trailing slash and mask.
+
+	Raises 
+    ------
+	TypeError if string is not parsable to either format. 
+	"""
 	if re.match('([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{2}', addr):
 		return CIDRBlock(addr)
 	if re.match('([0-9]{1,3}\.){3}[0-9]{1,3}', addr):
 		return IP(addr)
 	raise TypeError('Not a proper IP address')
+
+def test():
+	sample_ip = ip_parser('68.211.6.120')
+	sample_cidr = ip_parser('68.211.0.0/17')
+	print sample_ip.tostring()
+	print sample_cidr.tostring()
+	print sample_cidr.getmask()
+	print sample_cidr.getrange()
+
+if __name__ == '__main__':
+	test()
--- lpm_example.py
+++ lpm_example.py
@@ -6,13 +6,15 @@ def main():
 		                       '68.211.160.0/19', '68.211.192.0/18']))
 	
 	print 'address to match:'
-	print ips[0].tostring() + ': ' + ips[0].tobits() + '\n'
+	print ips[0].tostring() + '\n'
 	print 'possible matches:'
 	for i in range(1, len(ips)):
-		print ips[i].tostring() + ': ' + ips[i].tobits()
-		print 'matches ... ', ips[i].matches(ips[0]) 
+		print ips[i].tostring()
+		print 'first in range: ' + ips[i].getrange()[0]
+		print 'last in range: ' + ips[i].getrange()[1]
+		print 'matches ... ', ips[i].matches(ips[0]), '\n'
 
-	print '\n' + 'longest match: ' + \
+	print 'longest match: ' + \
 	      str(max([ips[0].lpm(ips[i]) for i in range(1, len(ips))]))
 
 if __name__ == '__main__':
